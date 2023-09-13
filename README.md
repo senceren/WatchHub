688fd0b92da5971f484fca5ac95e951cce4a3f0c
senceren <cerensen222@gmail.com>
2023-09-13 10:23:39 +0300

    Entities and IRepository

---
+++ README.md
@@ -0,0 +1,11 @@
+# WatchHub
+
+A Sample N-Layered .NET Core Project
+Demonstrating Clean Architecture and the Generic Repository Pattern.
+
+## Packages
+
+### Application Core
+```
+Install-Package Ardalis.Specification
+````
---
+++ src/ApplicationCore/Interfaces/IRepository.cs
@@ -0,0 +1,29 @@
+﻿using ApplicationCore.Entities;
+using Ardalis.Specification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Interfaces
+{
+    // Burda verilerin nasıl gösterileceğine dair bilgiler yer alır.
+    // Generic Repository Pattern 
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<T?> GetByIdAsync(int id);
+        Task<List<T>> GetAllAsync();
+
+        // belirli sınırlarda listemek ve getirmek için
+        Task<List<T>> GetAllAsync(ISpecification<T> specification);  
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity); // geri döndürmeye gerek yok
+        Task DeleteAsync(T entity);
+
+        // 300 dolardan fazla olan ürünlerin sayısını getir.
+        Task<int> CountAsync(ISpecification<T> specification);
+        Task<T> FirstAsync(ISpecification<T> specification);
+        Task<T?> FirstOrDefaultAsync(ISpecification<T> specification);
+    }
+}
